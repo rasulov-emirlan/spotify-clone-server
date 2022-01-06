cccfe57eaaa8fbbb32f4981378b582414ca1217e
rasulov-emirlan <rasulov.emirlan@gmail.com>
2022-01-06 20:36:46 +0600

    migration update

--- migrations/000012_favorite_authors.up.sql
+++ migrations/000012_favorite_authors.up.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS favorite_authors (
 	CONSTRAINT fk_favorite_authors_user_id FOREIGN KEY (user_id)
 		REFERENCES users(id),
 	CONSTRAINT fk_favorite_authors_author_id FOREIGN KEY (author_id)
-		REFERENCES users(id)
+		REFERENCES users(id),
+	CONSTRAINT favorite_authors_user_id_cannot_be_author_id
+		CHECK (user_id <> author_id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_favorite_authors
